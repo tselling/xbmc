202b660b72f5f09bbca2928646aa35d265ddfa65
Mike C <mcrosson_git at nusku dot net>
2012-08-25 01:42:34 -0400

    Jenkins artifact copy

--- publish.sh
+++ publish.sh
@@ -2,6 +2,7 @@
 
 REV=`git log -n1 --pretty=oneline | cut -f1 -d' '`
 TYPE=$1
+SRC=$2
 
 echo $TYPE
-cp ./xbmcapp-armeabi-v7a-debug.apk ~/xbmc/dist/xbmcapp-armeabi-v7a-debug-${REV}-${TYPE}.apk
+cp ${SRC}/xbmcapp-armeabi-v7a-debug.apk /scratch/xbmc/dist/xbmcapp-armeabi-v7a-debug-${REV}-${TYPE}.apk
