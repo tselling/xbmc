8020731b35b0084ed8741bd13ad77c1070d6850e
Mike C <mcrosson_git at nusku dot net>
2012-09-02 13:40:15 -0400

    Improved file naming convention

---
+++ current_date.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+DATE=`date +%Y-%m-%d`
+
+echo $DATE
--- publish.sh
+++ publish.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 REV=`git log -n1 --pretty=oneline | cut -f1 -d' '`
+DATE=`date +%Y-%m-%d`
 TYPE=$1
 SRC=$2
 
 echo $TYPE
 echo $REV
-cp ${SRC}/xbmcapp-armeabi-v7a-debug.apk /scratch/xbmc/dist/xbmcapp-armeabi-v7a-debug-${REV}-${TYPE}.apk
+cp ${SRC}/xbmcapp-armeabi-v7a-debug.apk /scratch/xbmc/dist/xbmcapp-armeabi-v7a-debug-${DATE}-${TYPE}-${REV}.apk
