5b366da9160ed97bfd0b7a432f0d2a4772c0bcef
Mike C <mcrosson_git at nusku dot net>
2013-01-11 22:18:08 -0500

    Updated NDK

--- build_deps.sh
+++ build_deps.sh
@@ -16,9 +16,9 @@ cd tools/android/depends
 ./bootstrap || exit 1
 
 ./configure \
-        --with-ndk=${TOOLS_PREFIX}/android-ndk-r7-crystax-5.beta2 \
+        --with-ndk=${TOOLS_PREFIX}/android-ndk-r7-crystax-5.beta3 \
         --with-sdk=${TOOLS_PREFIX}/android-sdk-linux \
-        --with-toolchain=${TOOLS_PREFIX}/android-sdk-linux/android-9-external_player \
+        --with-toolchain=${TOOLS_PREFIX}/android-sdk-linux/android-9-xaf-touch \
         --with-tarballs=${TOOLS_PREFIX}/tarballs \
         --with-sdk-platform=android-9 || exit 1
 
