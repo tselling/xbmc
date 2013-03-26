242c87aaaf75461684b3751264df90f9dc64c1fe
Mike C <mcrosson_git at nusku dot net>
2013-03-26 19:29:05 +0000

    Updated build scripts for new depends system
    Conflicts:
    
    	build_deps.sh

--- build_deps.sh
+++ build_deps.sh
@@ -13,18 +13,20 @@ cd $BUILD_DIR
 
 rm -rf addons/skin.touched
 
-cd tools/android/depends
+cd tools/depends
 
 ./bootstrap || exit 1
 
 ./configure \
         --with-ndk=${TOOLS_PREFIX}/android-ndk-r7-crystax-5.beta3 \
-        --with-sdk=${TOOLS_PREFIX}/android-sdk-linux \
+        --with-sdk-path=${TOOLS_PREFIX}/android-sdk-linux \
         --with-toolchain=${TOOLS_PREFIX}/android-sdk-linux/android-9-xaf-set_top \
         --with-tarballs=${TOOLS_PREFIX}/tarballs \
-        --with-sdk-platform=android-9 || exit 1
+        --prefix=${TOOLS_PREFIX}/prefixes/android-9-neon-xaf-set_top \
+		--host=arm-linux-androideabi \
+        --with-sdk=android-9 || exit 1
 
 make -j10
 make
 
-cd ../../../
+cd ../../
