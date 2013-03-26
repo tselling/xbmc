c07e0b9107fd78da422b08f9cc2a4713b599c507
Mike C <mcrosson_git at nusku dot net>
2013-03-26 21:35:08 +0000

    Updated build scripts

--- build_deps.sh
+++ build_deps.sh
@@ -11,18 +11,20 @@ echo $TYPE
 
 cd $BUILD_DIR
 
-cd tools/android/depends
+cd tools/depends
 
 ./bootstrap || exit 1
 
 ./configure \
         --with-ndk=${TOOLS_PREFIX}/android-ndk-r7-crystax-5.beta2 \
-        --with-sdk=${TOOLS_PREFIX}/android-sdk-linux \
+        --with-sdk-path=${TOOLS_PREFIX}/android-sdk-linux \
         --with-toolchain=${TOOLS_PREFIX}/android-sdk-linux/android-9-xaf-touch \
         --with-tarballs=${TOOLS_PREFIX}/tarballs \
-        --with-sdk-platform=android-9 || exit 1
+        --prefix=${TOOLS_PREFIX}/prefixes/android-9-tegra2-xaf \
+		--host=arm-linux-androideabi \
+        --with-sdk=android-9 || exit 1
 
 make -j10
 make
 
-cd ../../../
+cd ../../
